b946328f056a34929a771efeaf81aca01bbee504
User <relm7989@gmail.com>
2025-08-09 11:12:04 +0900

    기본 데이터 형태 학습

---
+++ x.py
@@ -0,0 +1,65 @@
+x=1
+print(x+3)
+
+x=3.5
+print(x)
+
+
+x=3+5
+print(x)
+
+
+x=3==5
+print(x)
+
+
+x=True
+print(x)
+
+
+x = [1,2,3,4,5]#list
+print(x[1])
+
+x = {'a': 100, 'b': 200}
+print(x['a'])
+print(type(x))
+
+example = {
+
+    'python': [True, False, True, True, True, True, True, False, False, True],
+
+    'java': [True, False, False, True, True, True, False, False, False, True],
+
+    'git': [False, False, True, True, False, True, True, True, True, True],
+
+}
+
+python_description = [
+
+    {
+
+        'answer': 1,
+
+        'description': 'python에 대한 설명은 1번이 맞습니다.'
+
+    },
+
+    {
+
+        'answer': "list",
+
+        'description': 'python의 열거형 데이터 타입은 list입니다.'
+
+    },
+
+    {
+
+        'answer': True,
+
+        'description': 'python의 LIST 안에 Dictionary를 사용할 수 있습니다.'
+
+    },
+
+]
+
+list_example = [1, "+", 2, "="]
