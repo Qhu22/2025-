01886acd699163134cbb38ea8a52e5a363b0003b
User <relm7989@gmail.com>
2025-08-09 11:43:00 +0900

    사용자 입력 및 출력 실습

--- gpg.py
+++ gpg.py
@@ -1,2 +1,10 @@
 print(1)
 #안녕하세요
+
+#사용자에게 입력받은 값을 계산중에 정수로 바꾼것
+User_input = input("숫자를 입력하고 엔터를 누르세요")
+print(int(User_input)+5)
+
+#사용자에게 입력받은 값을 바로 정수로 바꾼것
+User_input = int(input("숫자를 입력하고 엔터를 누르세요"))
+print(User_input+5)
