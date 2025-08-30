68439e621e87edd20643f6d58a94232c4c404fc9
User <relm7989@gmail.com>
2025-08-30 10:08:00 +0900

    논리연산자 및 연산자 복합 사용

--- operator.py
+++ operator.py
@@ -47,3 +47,41 @@ print("str_x + str_y :", str_z )
 #print("str_x - str-y" , str_z)
 str_z = str_x * 2
 print("str_x * 2 :", str_z)
+
+#delayed_effect_skill_a_ = "" # snake_case
+#delayedeffectskilla ='' #camel case
+
+array_x = [3, 6]
+array_y = [4, 5]
+
+array_z = array_x + array_y
+print("array_x + array_y", array_z)
+
+array_z = array_x * 2
+print("array_x * 2", array_z)
+
+array_z = array_x * array_y[0]
+print("array_x + array_y[0] :", array_z)
+
+#논리 연산자
+report_card = {
+    "국어": 3,
+    "수학": 2,
+    "영어": 6,
+    "물리": 1,
+    "화학": 2,
+    "생명과학": 1
+}
+
+can_apply = report_card["국어"] < 3 and report_card["수학"] < 3 and report_card["영어"] < 3
+
+print("지원가능여부:", can_apply)
+
+# 3합 6
+tatal_score = report_card["국어"]+report_card["수학"]+report_card["영어"] <=6
+print("3합 6:", tatal_score)
+
+#과학 영재반
+can_apply_class_for_the_gifted = report_card["물리"] == 1 or report_card["화학"] ==1 or report_card["생명과학"]
+print("영재반 지원가능여부", can_apply_class_for_the_gifted)
+
