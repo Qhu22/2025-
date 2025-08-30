8ce5647fdc7195177bad5cbf0b3cc1b6e0e76ac4
User <relm7989@gmail.com>
2025-08-30 10:50:09 +0900

    list 가공하는 방법

--- x.py
+++ x.py
@@ -19,6 +19,39 @@ print(x)
 
 x = [1,2,3,4,5]#list
 print(x[1])
+print(type(x))
+print(x[3])
+
+x = [1,2,3,4,5]
+x= x * 2
+print("x * 2", x)
+#slicing
+print(x[2:5]) # [start_index : end_index+1]
+print("append======")
+x.append(7)
+print(x)
+print("insert======")
+x.insert(1,9)
+print(x)
+print("remove======")#인덱스가 아닌 값을 지우는 함수
+print(x)
+print("9->6=========")
+x[0] = 6
+print(x)
+
+#참조 할 수 없는 위치를 참조했을때 (배열길이 이상)
+#IndexError: list assignment index  out of range
+#x[7] = 1
+print("배열의 길이=========")
+print(len(x))
+
+print("마지막 배열의 인덱스")
+print(len(x)-1)
+
+
+
+
+
 
 x = {'a': 100, 'b': 200}
 print(x['a'])
@@ -70,3 +103,19 @@ dict_example = {
     1:'asdf'#key에 변수,함수는 사용불가하지만 상수, 문자는 가능하다 ,
     ,'a':'asdf'
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
