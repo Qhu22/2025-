e6a8d5413d2c790093b57c5eaeec77123ce7fa04
User <relm7989@gmail.com>
2025-08-30 09:31:04 +0900

    숫자 형태의 연산

---
+++ operator.py
@@ -0,0 +1,37 @@
+x = 10 #대입연산자
+y = 15
+
+#조건연산자
+z = x == y
+print("x == y :", z)
+
+z = x != y
+print("x != y :", z)
+z = x > y
+print("x > y :", z)
+z = x >= y
+print("x >= y :", z)
+z = x < y
+print("x < y :", z)
+z = x <= y
+print("x <= y :", z)
+
+z = x + y
+print("x + y :", z)
+z = x - y
+print("x - y :", z)
+z = x * y
+print("x * y :", z)
+z = x / y
+print("x / y :", z)
+
+z = x % y
+print("x % y :", z)
+#나머지 출력
+
+z = x // y
+print("x // y :", z)
+#몫 출력
+
+z = x ** y
+print("x ** y", z) # 제곱
