3c3c8e53a069168d2e6feb782760dba15a0fb06e
User <relm7989@gmail.com>
2025-11-01 12:06:38 +0900

    피보나치수열 리스트 없이

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -30,12 +30,22 @@ while index <= input_number:
         print(index)
     index = index + 2
 
-
+print("피보나치 수열 withlist")
 list =[1,1]
 while list[-1] <= input_number:
     print(list[-1])
     list.append(list[-1] + list[-2])
 
+print("피보나치 수열 withoutlist")
+a=1
+b=1
+c=1
+
+while c <=input_number:
+    print(c)
+    c = a + b
+    a = b
+    b = c
 
 
 
