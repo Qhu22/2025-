a53c08c4b8c7319f18d2e7e688caae9a2581ea4f
User <relm7989@gmail.com>
2025-11-22 09:58:32 +0900

    함수 클린코드

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -1,12 +1,12 @@
-
-score = int(input("점수를 입력하세요"))
+def print_score_applied_or_not():
+    score = int(input("점수를 입력하세요"))
 
     if score > 100 or score <0:
-            #print("정상적인 점수 범위가 아닙니다")
+              print("정상적인 점수 범위가 아닙니다")
 
 
     else:
-       if score >= 80:
+        if score >= 80:
             print("합격입니다")
 
 
@@ -15,26 +15,29 @@ score = int(input("점수를 입력하세요"))
 
         else:
             print("불합격입니다")
+def print_what_was_that():
+    while True:
+        user_input = input("값을 입력하세요 : ")
 
-while True:
-    user_input = input("값을 입력하세요 : ")
+        if user_input.lower() == "z" :
+            break
 
-    if user_input.lower() == "z" :
-        break
+    input_number =int(input("숫자를 입력하세요."))
+    index = 2
 
-input_number =int(input("숫자를 입력하세요."))
-index = 2
+    while index <= input_number:
+        if index % 2 == 0:
+            print(index)
+        index = index + 2
 
-while index <= input_number:
-    if index % 2 == 0:
-        print(index)
-    index = index + 2
 
 print("피보나치 수열 withlist")
-list =[1,1]
-while list[-1] <= input_number:
-    print(list[-1])
-    list.append(list[-1] + list[-2])
+def print_fibonacci_with_list():
+    input_number = int(input("숫자를 입력하세요."))
+    list = [1, 1]
+    while list[-1] <= input_number:
+        print(list[-1])
+        list.append(list[-1] + list[-2])
 
 print("피보나치 수열 withoutlist")
 def print_fibonacci_type2():
@@ -43,11 +46,11 @@ def print_fibonacci_type2():
     b=1
     c=1
 
-while c <=input_number:
-    print(c)
-    c = a + b
-    a = b
-    b = c
+    while c <=input_number:
+        print(c)
+        c = a + b
+        a = b
+        b = c
 
 
 
