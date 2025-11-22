427cecf5c97ab7982c3ed65e1015e618dc825beb
User <relm7989@gmail.com>
2025-11-22 09:37:05 +0900

    주석대신 함수로 반복문

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -1,26 +1,26 @@
 
-#score = int(input("점수를 입력하세요"))
+score = int(input("점수를 입력하세요"))
 
-#    if score > 100 or score <0:
-        #print("정상적인 점수 범위가 아닙니다")
-
-
-#    else:
-#       if score >= 80:
-#            print("합격입니다")
+    if score > 100 or score <0:
+            #print("정상적인 점수 범위가 아닙니다")
 
 
-#        elif score >=60:
-#            print("재시험 응시가 필요합니다")
+    else:
+       if score >= 80:
+            print("합격입니다")
 
-#        else:
-#            print("불합격입니다")
 
-#while True:
-#    user_input = input("값을 입력하세요 : ")
-#
- #   if user_input.lower() == "z" :
-  #      break
+        elif score >=60:
+            print("재시험 응시가 필요합니다")
+
+        else:
+            print("불합격입니다")
+
+while True:
+    user_input = input("값을 입력하세요 : ")
+
+    if user_input.lower() == "z" :
+        break
 
 input_number =int(input("숫자를 입력하세요."))
 index = 2
@@ -37,9 +37,11 @@ while list[-1] <= input_number:
     list.append(list[-1] + list[-2])
 
 print("피보나치 수열 withoutlist")
-a=1
-b=1
-c=1
+def print_fibonacci_type2():
+    input_number = int(input("숫자를 입력하세요"))
+    a=1
+    b=1
+    c=1
 
 while c <=input_number:
     print(c)
