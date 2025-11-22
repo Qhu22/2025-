663c4fe78b1e970b940efe250ac2061ebfd10823
User <relm7989@gmail.com>
2025-11-22 11:49:00 +0900

    여러 종류의 데이터를 분석해서 그에 맞는 값을 출력하기

--- 3. control-statement.py
+++ 3. control-statement.py
@@ -88,3 +88,102 @@ for (student, correct) in zip(a,correct_answer):
         final_score =final_score-10
 
 print(final_score)
+
+
+
+
+
+
+
+test = [[1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+ [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+ [1, 5, 3, 4, 5, 1, 2, 3, 1, 2],
+ [4, 3, 4, 4, 5, 3, 1, 2, 2, 4],
+ [1, 3, 4, 4, 5, 3, 1, 2, 3, 2],
+ [1, 3, 5, 4, 5, 3, 1, 2, 3, 4]]
+def get_score(student_answer, correct_answer):
+    # 학생의 점수구하기 한 문항당 10점이라 가정.
+    score = 0
+    for (student, correct) in zip(student_answer, correct_answer):
+        if student == correct:
+            score = score + 10
+    return score
+a=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+score = get_score(a, correct_answer)
+print(score)
+
+test = [
+    {
+        'name': 'aaa',
+        'number': 10101,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'bbb',
+        'number': 10102,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ccc',
+        'number': 10103,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'ddd',
+        'number': 10104,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    },
+    {
+        'name': 'eee',
+        'number': 10105,
+        'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+        'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    }
+]
+
+correct_answer = {
+    'math': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'korean': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'english': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+    'science': [1, 3, 2, 4, 5, 3, 1, 2, 3, 4],
+}
+
+for student in test:
+    math_score = get_score(student['math'], correct_answer['math'])
+    korean_score = get_score(student['korean'], correct_answer['korean'])
+    english_score = get_score(student['english'], correct_answer['english'])
+    science_score = get_score(student['science'], correct_answer['science'])
+
+    print("수학점수:", math_score)
+    print("국어점수:", korean_score)
+    print("영어점수:", english_score)
+    print("과학점수:", science_score)
+
+    print("학생", student['name'], "==================")
+
+    # for 중첩
+    # student.keys(), (이름이거나, 학번인 경우) or (배열이 아닌경우) continue
+
+    math_score = get_score(student['math'], correct_answer['math'])
+    korean_score = get_score(student['korean'], correct_answer['korean'])
+    english_score = get_score(student['english'], correct_answer['english'])
+    science_score = get_score(student['science'], correct_answer['science'])
+
+    print("수학점수:", math_score)
+    print("국어점수:", korean_score)
+    print("영어점수:", english_score)
+    print("과학점수:", science_score)
