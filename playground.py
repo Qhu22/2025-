c713307209a6fe293d302382c5d477f2b312f641
User <relm7989@gmail.com>
2025-11-29 10:33:48 +0900

    d업다운  3 고치는 중

--- playground.py
+++ playground.py
@@ -1,43 +1,39 @@
 import random
-def print_times_table(number):
-    print(number, "*", 1, "=", number * 1)
-
-    print(number, "*", 2, "=", number * 2)
-
-    print(number, "*", 3, "=", number * 3)
-
-    print(number, "*", 4, "=", number * 4)
-
-    print(number, "*", 5, "=", number * 5)
-
-    print(number, "*", 6, "=", number * 6)
-
-    print(number, "*", 7, "=", number * 7)
-
-    print(number, "*", 9, "=", number * 9)
+#def print_times_table(number):
+ #   print(number, "*", 1, "=", number * 1)
+#
+ #   print(number, "*", 2, "=", number * 2)
+#
+ #   print(number, "*", 3, "=", number * 3)
+#
+ #   print(number, "*", 4, "=", number * 4)
+#
+ #   print(number, "*", 5, "=", number * 5)
+##### print(number, "*", 9, "=", number * 9)
 
 
+#while True:
+
+ #   user_input = input("값을 입력하세요 : ")
+
+  #  if user_input.lower() == "z":
+   #     break
+
+        
+    #print_times_table(int(user_input))
+
+
+x = (int(input("값을 입력하세요")))
+the_number = int(random.random())
 while True:
+    if x > the_number:
+        print("down")
 
-    user_input = input("값을 입력하세요 : ")
 
-    if user_input.lower() == "z":
+    elif x < the_number:
+        print("up")
+
+    elif x == the_number:
+        print("complete!!")
         break
-        
-        
-    print_times_table(int(user_input))
-
-
-def updown ():
-    the_number = int(random.random())
-    while
-        x= int(input())
-        if x > the_number:
-            print("down")
-
-        if x < the_number:
-            print("up")
-
-        if x == the_number:
-            print("complete!!")
 
