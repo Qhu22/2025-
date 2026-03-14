88aea36ea398b7902ec7e1762a08909f07fbaa61
User <relm7989@gmail.com>
2026-03-14 11:47:33 +0900

    step1 complete

--- my rpg.py
+++ my rpg.py
@@ -1,32 +1,54 @@
+import random
 from random import randint
-
+boss_level = 0
+boss_name = ['슬라임', '오크', '']
 print("게임에 오신것을 환영합니다!")
-weapon_level = 0
-level0_rate = {"up": 70, "keep": 30, "down":0, "break":0}
-level1_rate = {"up": 60, "keep": 25, "down":10, "break": 5}
-level2_rate = {"up": 50, "keep": 25, "down":10, "break": 5}
-level3_rate = {"up": 45, "keep": 25, "down":10, "break": 5}
-level4_rate = {"up": 40, "keep": 25, "down":10, "break": 5}
-level5_rate = {"up": 35, "keep": 25, "down":10, "break": 5}
-level6_rate = {"up": 30, "keep": 25, "down":10, "break": 5}
-level7_rate = {"up": 25, "keep": 25, "down":10, "break": 5}
-level8_rate = {"up": 20, "keep": 25, "down":10, "break": 5}
-level9_rate = {"up": 10, "keep": 25, "down":10, "break": 5}
-level10_rate = {"up": 0, "keep": 100, "down":0, "break": 0}
-upgrade_rates =[level0_rate, level1_rate, level2_rate, level3_rate, level4_rate, level5_rate, level6_rate, level7_rate,
-                level8_rate, level9_rate, level10_rate]
 
+upgrade_matrix = [
+    [70, 30,  0,  0],
+    [60, 25, 10,  5],
+    [50, 30, 15,  5],
+    [45, 30, 20,  5],
+    [40, 30, 20, 10],
+    [35, 30, 25, 10],
+    [30, 30, 30, 10],
+    [25, 30, 30, 15],
+    [20, 30, 30, 20],
+    [15, 30, 30, 25],
+    [ 0, 100,  0,  0]
+]
+
+weapon_level = 0
+outcomes = ["up", "keep", "down", "break"]
+current_weights = upgrade_matrix[weapon_level]
+result = random.choices(outcomes,weights = current_weights)[0]
 while True:
-    choice = input("숫자를 입력 하세요:   0.종료     1.무기강화")
+    choice = input("숫자를 입력 하세요:   0.종료     1.무기강화       2.보스도전")
     if choice == "0" :
         break
 
     elif choice == "1" :
         reinforcing = input("무기를 강화 하시겠습니까? Y or N")
         if reinforcing == "Y":
-            print("무기강화를 시작합니다")
-            ran_num = randint(range(1, 99))
-            if ran_num <= 59:
+            print("강화를 시작합니다")
+            if result == "up":
+                print("강화성공")
+                weapon_level = weapon_level + 1
+
+            elif result == "keep":
+                print("강화실패:레벨유지")
+
+            elif result == "down":
+                print("강화실패:레벨하락")
+                weapon_level = weapon_level -1
+
+            elif result == "break":
+                print("강화실패:무기파괴")
+                weapon_level = 0
+
+
+    elif choice == "2":
+        print("보스도전")
 
 
 
